11e78771da3f4067001d7b105c522de45e824dec
dvidalv <dvidalv@gmail.com>
2024-03-11 13:42:03 -0500

    Refactor email sending code in api-mail.js

--- api/api-mail.js
+++ api/api-mail.js
@@ -5,30 +5,30 @@ sgMail.setApiKey(key);
 
 const sendMail = async (req, res) => {
   const { email, subject, message } = req.body;
-  console.log(email, subject, message)
   const msg = {
     to: email, // Change to your recipient
     from: 'dvidalv@gmail.com', // Change to your verified sender
     subject: subject,
     content: [
       {
-        type: "text/html",
-        value: message // Assuming 'email' variable contains the email body text.
-      }
-    ]
+        type: 'text/html',
+        value: message, // Assuming 'email' variable contains the email body text.
+      },
+    ],
   };
   sgMail
     .send(msg)
     .then(() => {
-      console.log('Email sent');
       res.status(200).json({ status: 'success', message: 'Email enviado' });
     })
     .catch((error) => {
       console.error(error.response.body);
-      res.status(500).json({ status: 'error', message: 'Error al enviar el email', details: error.response.body });
+      res.status(500).json({
+        status: 'error',
+        message: 'Error al enviar el email',
+        details: error.response.body,
+      });
     });
-
 };
 
 module.exports = sendMail;
-
