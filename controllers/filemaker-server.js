6a1e5bfae3d23443ded797d0787e61ba31e64f7e
David Vidal <dvidalv@gmail.com>
2024-06-22 15:15:55 -0500

    fixed small bugs

--- controllers/filemaker-server.js
+++ controllers/filemaker-server.js
@@ -16,7 +16,7 @@ const getFilemakerToken = async (req, res) => {
       body: JSON.stringify({})
   });
   const data = await response.json();
-  // console.log('data', data);
+  console.log('data', data);
   return res.status(200).json(data);
 
 }
@@ -45,6 +45,9 @@ const getRecords = async (req, res) => {
 const getRecordByName = async (req, res) => {
   const token = req.body.token;
   const name = req.body.name;
+  console.log('req.body', req.body);
+  console.log('name', name);
+  console.log('token', token);
   const body = {
     query: [
       {
@@ -66,8 +69,40 @@ const getRecordByName = async (req, res) => {
       },
       body: JSON.stringify(body)
   });
+  // console.log('response', response);
   const data = await response.json();
   return res.status(200).json(data);
 }
 
-module.exports = { getFilemakerToken, getRecords, getRecordByName };
+// Descargar archivo PDF de un contenedor
+const downloadPDF = async (req, res) => {
+  const token = req.body.token; // Asegúrate de que el token es válido y está activo
+  const recordId = req.body.id; // ID del registro donde se encuentra el contenedor
+  const fieldName = "Url_Resultado"; // Nombre del campo contenedor
+
+  const url = `${FILEMAKER_URL}/fmi/data/vLatest/databases/${FILEMAKER_DATABASE}/layouts/${FILEMAKER_LAYOUT}/records/${recordId}/containers/${fieldName}/1`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET', // Usando GET para descargar
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    console.log('response', response);
+    if (response.ok) {
+      const blob = await response.blob();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename="downloaded.pdf"');
+      blob.stream().pipe(res);
+    } else {
+      throw new Error(`Failed to download PDF: ${response.statusText}`);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { getFilemakerToken, getRecords, getRecordByName, downloadPDF };
+
+// module.exports = { getFilemakerToken, getRecords, getRecordByName };
--- routes/fileMaker.js
+++ routes/fileMaker.js
@@ -3,6 +3,7 @@ const {
   getFilemakerToken,
   getRecords,
   getRecordByName,
+  downloadPDF
 } = require('../controllers/filemaker-server');
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get('/', getFilemakerToken);
 router.post('/records', getRecords);
 router.post('/record', getRecordByName);
+router.post('/download', downloadPDF);
 
 module.exports = router;
